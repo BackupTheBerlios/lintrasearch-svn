63914a989825d913dc7eec66a663fa86fd439825
pr0gm4 <pr0gm4@401b67de-4b08-0410-a655-a967404fc6cc>
2005-12-26 22:19:22 +0000

    windows startbatch und bugfixes in linux startscripts

---
+++ trunk/startClient.bat
@@ -0,0 +1,2 @@
+java -classpath .\lib\jaxen-core.jar;.\lib\jaxen-jdom.jar;.\lib\saxpath.jar;.\lib\openide-loaders.jar;.\lib\openide.jar;.\lib\AbsoluteLayout.jar;.\lib\jdom.jar;.\lib\org.linoratix.configfilereader.jar;.\lib\org.linoratix.base64.jar;.\lib\activation.jar;.\ContentClient\dist\ContentClient.jar contentclient.ContentClient C:\Programme\lIntraSearch\install\client.xml
+
